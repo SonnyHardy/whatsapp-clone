85b2d086d38011b65759c5d7317e754af1c3cc67
SonnyHardy <htambeck@gmail.com>
2025-01-13 22:48:59 +0100

    Initial Spring Boot project

---
+++ whatsappclone/whatsappclone/src/test/java/com/sonny/whatsappclone/WhatsAppCloneApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sonny.whatsappclone;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WhatsAppCloneApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
