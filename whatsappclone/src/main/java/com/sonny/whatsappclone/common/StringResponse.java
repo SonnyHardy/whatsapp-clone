794c68f0fff00a054f8c0c241a51d41880b80c72
SonnyHardy <htambeck@gmail.com>
2025-01-14 12:16:32 +0100

    Implementation of the chat domain

---
+++ whatsappclone/src/main/java/com/sonny/whatsappclone/common/StringResponse.java
@@ -0,0 +1,13 @@
+package com.sonny.whatsappclone.common;
+
+import lombok.*;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class StringResponse {
+
+    String response;
+}
