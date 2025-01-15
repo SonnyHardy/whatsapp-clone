89c575bc464b1f3b3cd4a9a6df99ab196ed752e9
SonnyHardy <htambeck@gmail.com>
2025-01-15 21:11:40 +0100

    Implement the main interface

---
+++ whatsapp-clone-ui/src/app/components/chat-list/chat-list.component.ts
@@ -0,0 +1,15 @@
+import {Component, input, InputSignal} from '@angular/core';
+import {ChatResponse} from "../../services/models/chat-response";
+
+@Component({
+  selector: 'app-chat-list',
+  imports: [],
+  templateUrl: './chat-list.component.html',
+  standalone: true,
+  styleUrl: './chat-list.component.scss'
+})
+export class ChatListComponent {
+
+  chats: InputSignal<ChatResponse[]> = input<ChatResponse[]>([]);
+
+}
