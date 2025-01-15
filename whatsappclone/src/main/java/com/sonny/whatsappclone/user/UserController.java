f94fac9aa237bf3cd21602cdff85385e518ebd45
SonnyHardy <htambeck@gmail.com>
2025-01-15 11:53:57 +0100

    Implementation of the user domain

---
+++ whatsappclone/src/main/java/com/sonny/whatsappclone/user/UserController.java
@@ -0,0 +1,23 @@
+package com.sonny.whatsappclone.user;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.Authentication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping(path = "/api/v1/users")
+public class UserController {
+
+    private final UserService userService;
+
+    @GetMapping
+    public ResponseEntity<List<UserResponse>> getAllUsers(Authentication authentication) {
+        return ResponseEntity.ok(userService.getAllUsersExceptSelf(authentication));
+    }
+}
