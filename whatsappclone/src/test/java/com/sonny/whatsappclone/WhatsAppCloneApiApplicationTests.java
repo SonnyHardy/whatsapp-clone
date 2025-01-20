5ec28fb82c44061192f8ef08ae93515823b2cfcb
SonnyHardy <htambeck@gmail.com>
2025-01-20 17:23:06 +0100

    CI/CD Pipelines

--- whatsappclone/src/test/java/com/sonny/whatsappclone/WhatsAppCloneApiApplicationTests.java
+++ whatsappclone/src/test/java/com/sonny/whatsappclone/WhatsAppCloneApiApplicationTests.java
@@ -2,8 +2,10 @@ package com.sonny.whatsappclone;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class WhatsAppCloneApiApplicationTests {
 
 	@Test
