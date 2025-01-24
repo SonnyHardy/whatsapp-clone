9b626bd4fb886f2dc3b9f9b62d43f0f705031962
SonnyHardy <htambeck@gmail.com>
2025-01-24 20:01:09 +0100

    CI/CD Pipelines

---
+++ whatsapp-clone-ui/src/environments/environment.development.ts
@@ -0,0 +1,7 @@
+export const environment = {
+  production: false,
+  apiUrl: 'http://localhost:8088',
+  websocketUrl: 'http://localhost:8088/websocket',
+  keycloakUrl: 'http://localhost:9090',
+  redirectUri: 'http://localhost:4200'
+};
---
+++ whatsapp-clone-ui/src/environments/environment.production.ts
@@ -0,0 +1,7 @@
+export const environment = {
+  production: true,
+  apiUrl: 'https://whatsapp-clone-api.sonnydevtest.de',
+  websocketUrl: 'https://whatsapp-clone-api.sonnydevtest.de/websocket',
+  keycloakUrl: 'https://whatsapp-clone-auth.sonnydevtest.de',
+  redirectUri: 'https://whatsapp-clone.sonnydevtest.de'
+};
