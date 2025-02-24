df0551718012c14a3b0c5c94c4cb0a4c74f3adc2
SonnyHardy <htambeck@gmail.com>
2025-02-24 20:19:56 +0100

    Ajustment apiUrl in frontend

--- whatsapp-clone-ui/src/environments/environment.production.ts
+++ whatsapp-clone-ui/src/environments/environment.production.ts
@@ -1,6 +1,7 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://whatsapp-clone-api.sonnydevtest.de',
+  //apiUrl: 'https://whatsapp-clone-api.sonnydevtest.de',
+  apiUrl: 'https://whatsapp-clone-api-yh11.onrender.com',
   websocketUrl: 'https://whatsapp-clone-api.sonnydevtest.de/websocket',
   keycloakUrl: 'https://whatsapp-clone-auth.sonnydevtest.de',
   redirectUri: 'https://whatsapp-clone.sonnydevtest.de'
