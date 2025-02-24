8e1239a097156a2adb2dbcff3458b40f708f5cf1
SonnyHardy <htambeck@gmail.com>
2025-02-24 23:48:20 +0100

    Ajustment apiUrl in frontend (from Google Cloud Run)

--- whatsapp-clone-ui/src/environments/environment.production.ts
+++ whatsapp-clone-ui/src/environments/environment.production.ts
@@ -1,7 +1,8 @@
 export const environment = {
   production: true,
-  //apiUrl: 'https://whatsapp-clone-api.sonnydevtest.de',
-  apiUrl: 'https://whatsapp-clone-api-yh11.onrender.com',
+  //apiUrl: 'https://whatsapp-clone-api.sonnydevtest.de',     // from OVH VPS
+  //apiUrl: 'https://whatsapp-clone-api-yh11.onrender.com',   // from Render
+  apiUrl: 'https://whatsapp-clone-api-488816087046.europe-west3.run.app',    // from Google Cloud Run
   websocketUrl: 'https://whatsapp-clone-api.sonnydevtest.de/websocket',
   keycloakUrl: 'https://whatsapp-clone-auth.sonnydevtest.de',
   redirectUri: 'https://whatsapp-clone.sonnydevtest.de'
